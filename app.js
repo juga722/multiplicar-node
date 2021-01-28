a3c95bc4d1a01c34bba77849b933a7c0577be26d
AJG <juga722@gmail.com>
2021-01-27 20:30:17 -0600

    Commit inicial

---
+++ app.js
@@ -0,0 +1,39 @@
+const argv = require('./config/yargs').argv;
+var colors = require('colors');
+
+const { crearArchivo,listarTabla } = require('./multiplicar/multiplicar')
+
+
+let comando = argv._[0];
+
+switch(comando){
+
+  case 'listar':
+    listarTabla(argv.base, argv.limite);
+    break;
+  case 'crear':
+      crearArchivo(argv.base, argv.limite)
+        .then( archivo => console.log(`Archivo creado: ${ archivo }`) )
+        .catch(e => console.log(e));
+    break;
+  default:
+    console.log('Comando no reconocido');
+  
+
+}
+
+// const fs = require('express');
+// const fs = require('./fs');
+
+// console.log( process.argv );
+// let argv2 = process.argv;
+
+// console.log('Limite', argv.limite);
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+
+  // crearArchivo(base)
+  //   .then( archivo => console.log(`Archivo creado: ${ archivo }`) )
+  //   .catch(e => console.log(e));
